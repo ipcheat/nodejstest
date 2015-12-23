d03cbf920aa45b2222b6d1304e99254ffa0e53ab
ipcheat <cuiming@yeah.net>
2015-12-23 17:55:22 +0800

    今天继续学习nodejs

---
+++ NodejsConsoleApp1/NodejsConsoleApp1/StringDecoder.js
@@ -0,0 +1,18 @@
+﻿var StringDecoder = require('string_decoder').StringDecoder;
+var decoder = new StringDecoder('utf8');
+
+//var cent = new Buffer([0xC2, 0xA2]);
+//console.log(decoder.write(cent));
+
+//var euro = new Buffer([0xE2, 0x82, 0xAC]);
+//console.log(decoder.write(euro));
+
+var fs = require("fs");
+fs.readFile('package.json', undefined, function (err, data) {
+
+    if (err)
+        console.log(err);
+    else
+        console.log(decoder.write(data));
+
+})
---
+++ NodejsConsoleApp1/NodejsConsoleApp1/modulepath.js
@@ -0,0 +1,10 @@
+﻿//console.log(module.paths);
+
+//var config = require("./package.json");
+//var config1 = require("./global.json");
+
+//console.log(config.name);
+//console.log(config1.mysqldb);
+
+var config2 = require(__dirname+'/global.json');
+console.log(config2.mysqldb);
